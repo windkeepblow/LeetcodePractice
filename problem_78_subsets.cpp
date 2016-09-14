d5b08c490e9084a61ea7b6b2429b5991b15dfd77
yifeng.shao <yifeng.shao@datayes.com>
2016-09-14 17:54:41 +0800

    accepted.

---
+++ problem_78_subsets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+    	sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        vector<int> buf;
+        subsets_helper(res, buf, nums, 0, nums.size());
+        return res;
+    }
+
+    void subsets_helper(vector<vector<int>>& res, vector<int>& buf, vector<int>& nums, int start, int size) {
+        res.push_back(buf);
+        for (int i = start; i < size; i++) {
+        	buf.push_back(nums[i]);
+        	subsets_helper(res, buf, nums, i + 1, size);
+        	buf.pop_back();
+        }
+    }
+};

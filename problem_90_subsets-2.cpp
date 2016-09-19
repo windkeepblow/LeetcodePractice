9c102eef8608724b454e320705209d6e4e33659c
yifeng.shao <yifeng.shao@datayes.com>
2016-09-19 19:57:48 +0800

    accepted.

--- problem_90_subsets-2.cpp
+++ problem_90_subsets-2.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        vector<int> buf;
+        subsets_helper(buf, res, nums, 0, nums.size());
+        return res;
+    }
+    
+private:
+    void subsets_helper(vector<int>& buf, vector<vector<int>>& res, vector<int>& nums, int start, int size) {
+        res.push_back(buf);
+        for (int i = start; i < size; i++) {
+        	buf.push_back(nums[i]);
+        	subsets_helper(buf, res, nums, i + 1, size);
+        	buf.pop_back();
+        	while (i + 1 < size && nums[i] == nums[i + 1]) i++;
+        }
     }
 };
